99c57441a87206ae16a322c4e7beb9b812ed4719
github Toshimatu <matsuda.akt@gmail.com>
2020-04-20 22:15:17 +0900

    [Add] taskモデルのテストを作成

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:task_a) { FactoryBot.create(:task) }
+
+  # titleが存在しなければ無効であること
+  it "is invalid without a title" do
+    task = Task.new(title: nil, status: 1)
+    task.valid?
+    expect(task.errors[:title]).to include("can't be blank")
+  end
+
+  # titleの値が重複していれば無効であること
+  it "is invalid with a duplicate task's title" do
+    task_a
+    task = Task.create(title: 'テストタイトル', status: 1)
+    task.valid?
+    expect(task.errors[:title]).to include("has already been taken")
+  end
+
+  # statusが存在しなければ無効であること
+  it "is invalid without a status" do
+    task = Task.new(title: 'テストタイトル', status: nil)
+    task.invalid?
+    expect(task.errors[:status]).to include("can't be blank")
+  end
 end
